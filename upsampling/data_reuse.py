c73e99bfcfc95a9a39bcbd0c04b69d536d48d091
Ian Colbert <26049551+icolbert@users.noreply.github.com>
2021-06-24 21:25:30 -0700

    Update data reuse arithmetic

--- upsampling/data_reuse.py
+++ upsampling/data_reuse.py
@@ -2,67 +2,101 @@ import numpy as np
 
 
 def sub_pixel_convolution_data_reuse_patterns(r, H, C, K):
-    M = (r**2) * (K**2) * (C**2) * (H**2)
-    W = (r**2) * (K**2) * (C**2)
-    A = (H**2) * C + (r**2) * (H**2) * C
-    P = 2 * (r**2) * (H**2) * C # post-processing
-    return M, W, A + P
+    M = pow(r, 2) * pow(K, 2) * pow(C, 2) * pow(H, 2)
+    W = pow(r, 2) * pow(K, 2) * pow(C, 2)
+    A = (1 + 3 * pow(r, 2)) * pow(H, 2) * C # plus post-processing
+    return M, W, A
 
 
 def NN_resize_convolution_data_reuse_patterns(r, H, C, K):
-    M = (r**2) * (K**2) * (C**2) * (H**2)
-    W = (K**2) * (C**2)
-    A = (H**2) * C + (r**2) * (H**2) * C
-    P = 2 * (r**2) * (H**2) * C # pre-processing
-    return M, W, A + P
+    M = pow(r, 2) * pow(K, 2) * pow(C, 2) * pow(H, 2)
+    W = pow(K, 2) * pow(C, 2)
+    A = (1 + 3 * pow(r, 2)) * pow(H, 2) * C # plus pre-processing
+    return M, W, A
 
 
-def reverse_looping_deconvolution_data_reuse_patterns(r, H, C, K, original_operator="D-SP"):
+def standard_deconvolution_data_reuse_patterns(r, H, C, K, original_operator="D-SP"):
     if original_operator == "D-SP":
         # Kd = Kc * r, S = r, P = r
-        M = (r**2) * (K**2) * (C**2) * (H**2)
-        W = (r**2) * (K**2) * (C**2)
-        A = (H**2) * C + (r**2) * (H**2) * C
+        M = pow(r, 2) * pow(K, 2) * pow(C, 2) * pow(H, 2)
+        W = pow(r, 2) * pow(K, 2) * pow(C, 2)
+        A = (1 + pow(r, 2)) * pow(H, 2) * C
         return M, W, A
     elif original_operator == "D-NN":
         # Kd = r + 2, S = r, P = 1
-        M = ((r + 2)**2) * (C**2) * (H**2)
-        W = ((r + 2)**2) * (C**2)
-        A = (H**2) * C + (r**2) * (H**2) * C
+        M = pow(r + 2, 2) * pow(C, 2) * pow(H, 2)
+        W = pow(r + 2, 2) * pow(C, 2)
+        A = (1 + pow(r, 2)) * pow(H, 2) * C
         return M, W, A
     else:
         raise NotImplementedError(f"{original_operator} is not yet supported.")
-    
+
 
 def fractionally_strided_deconvolution_data_reuse_patterns(r, H, C, K, original_operator="D-SP"):
     if original_operator == "D-SP":
         # Kd = Kc * r, S = r, P = r
-        M = (r**4) * (K**2) * (C**2) * (H**2)
-        W = (r**2) * (K**2) * (C**2)
-        A = ((H + (H - 1)*(r - 1))**2) * C + (r**2) * (H**2) * C
+        M = pow(r, 4) * pow(K, 2) * pow(C, 2) * pow(H, 2)
+        W = pow(r, 2) * pow(K, 2) * pow(C, 2)
+        A = (pow(H + (H - 1)*(r - 1), 2) + pow(r, 2) * pow(H, 2)) * C
         return M, W, A
     elif original_operator == "D-NN":
         # Kd = r + 2, S = r, P = 1
-        M = ((r + 2)**2) * (C**2) * (H**2) * (r**2)
-        W = ((r + 2)**2) * (C**2)
-        A = ((H + (H - 1)*(r - 1))**2) * C + (r**2) * (H**2) * C
+        M = pow(r + 2, 2) * pow(r, 2) * pow(H, 2) * pow(C, 2)
+        W = pow(r + 2, 2) * pow(C, 2)
+        A = (pow(H + (H - 1)*(r - 1), 2) + pow(r, 2) * pow(H, 2)) * C
         return M, W, A
     else:
         raise NotImplementedError(f"{original_operator} is not yet supported.") 
 
 
-def transforming_deconvolution_to_convolution_data_reuse_patterns(r, H, C, K, original_operator="D-SP"):
+def reverse_looping_deconvolution_data_reuse_patterns(r, H, C, K, original_operator="D-SP"):
     if original_operator == "D-SP":
         # Kd = Kc * r, S = r, P = r
-        M = (r**2) * (K**2) * (C**2) * (H**2)
-        W = (r**2) * (K**2) * (C**2)
-        A = (H**2) * C + (r**2) * (H**2) * C
+        M = pow(r, 2) * pow(K, 2) * pow(C, 2) * pow(H, 2)
+        W = pow(r, 2) * pow(K, 2) * pow(C, 2)
+        A = (1 + pow(r, 2)) * pow(H, 2) * C
         return M, W, A
     elif original_operator == "D-NN":
         # Kd = r + 2, S = r, P = 1
-        M = (r**2) * (np.ceil((r + 2) / r)**2) * (C**2) * (H**2)
-        W = (r**2) * (np.ceil((r + 2) / r)**2) * (C**2)
-        A = (H**2) * C + (r**2) * (H**2) * C
+        M = pow(r + 2, 2) * pow(C, 2) * pow(H, 2)
+        W = pow(r + 2, 2) * pow(C, 2)
+        A = (1 + pow(r, 2)) * pow(H, 2) * C
+        return M, W, A
+    else:
+        raise NotImplementedError(f"{original_operator} is not yet supported.")
+
+
+def reverse_looping_deconvolution_2_data_reuse_patterns(r, H, C, K, original_operator="D-SP"):
+    assert r >= 2, "upsampling factor needs to be >= 2"
+    if original_operator == "D-SP":
+        # Kd = Kc * r, S = r, P = r
+        M = pow(r, 2) * pow(K, 2) * pow(C, 2) * pow(H, 2)
+        W = pow(r, 2) * pow(K, 2) * pow(C, 2)
+        A = (1 + pow(r, 2)) * pow(H, 2) * C
+        return M, W, A
+    elif original_operator == "D-NN":
+        # Kd = r + 2, S = r, P = 1
+        M = 4 * pow(r, 2) * pow(C, 2) * pow(H, 2)
+        W = pow(r + 2, 2) * pow(C, 2)
+        A = (1 + pow(r, 2)) * pow(H, 2) * C
+        return M, W, A
+    else:
+        raise NotImplementedError(f"{original_operator} is not yet supported.")
+
+
+def transforming_deconvolution_to_convolution_data_reuse_patterns(r, H, C, K, original_operator="D-SP"):
+    assert r >= 2, "upsampling factor needs to be >= 2"
+    if original_operator == "D-SP":
+        # Kd = Kc * r, S = r, P = r
+        M = pow(r, 2) * pow(K, 2) * pow(C, 2) * pow(H, 2)
+        W = pow(r, 2) * pow(K, 2) * pow(C, 2)
+        A = (1 + pow(r, 2)) * pow(H, 2) * C
+        return M, W, A
+    elif original_operator == "D-NN":
+        # Kd = r + 2, S = r, P = 1
+        M = 4 * pow(r, 2) * pow(H, 2) * pow(C, 2)
+        W = 4 * pow(r, 2) * pow(C, 2)
+        A = (1 + pow(r, 2)) * pow(H, 2) * C
         return M, W, A
     else:
         raise NotImplementedError(f"{original_operator} is not yet supported.")
